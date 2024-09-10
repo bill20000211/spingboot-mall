f6a4204098e3bdbfb7a692c2590499aba0cac3f3
彭柏豪 <>
2024-09-10 19:02:07 +0800

    初始化 Spring Boot Mall 專案

---
+++ springboot-mall/src/test/java/com/bill/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

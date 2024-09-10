9af98623c62ca16045df45c82e45992050b98f12
彭柏豪 <>
2024-09-10 23:08:02 +0800

    商品功能 - Refactor category 使用 ENUM 類型

---
+++ springboot-mall/src/main/java/com/bill/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,9 @@
+package com.bill.springbootmall.constant;
+
+// 要用大寫，兩個單字用底線
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK,
+    OTHER
+}
--- springboot-mall/src/main/java/com/bill/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/bill/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.bill.springbootmall.rowmapper;
 
+import com.bill.springbootmall.constant.ProductCategory;
 import com.bill.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,13 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        // String 轉 Enum
+        // String categoryStr = rs.getString("category");
+        // ProductCategory productCategory = ProductCategory.valueOf(categoryStr);
+        // product.setCategory(productCategory);
+        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
